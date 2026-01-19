70c2d390bf6cd1311c6dfef9014ccc5b211ea26c
gamexdifficulty <gamexdifficulty@yahoo.com>
2026-01-19 08:59:09 +0100

    Changed duration of fade to color effect

--- firmware/lib/effects/include/effects/fadeToColor.h
+++ firmware/lib/effects/include/effects/fadeToColor.h
@@ -8,7 +8,7 @@ class FadeToColorEffect : public Effect {
 private:
     uint8_t fadeTo[3];
     uint8_t currentColor[3];
-    uint16_t duration = 1000;
+    uint16_t duration = 500;
 public:
     FadeToColorEffect(Leds& leds, uint8_t r, uint8_t g, uint8_t b);
     void update(uint32_t deltaTime, Leds& leds) override;
